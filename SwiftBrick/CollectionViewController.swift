28f26361991fb319084e4ecd798a0e8d8620d3da
Jackie <jackie>
2020-06-18 17:24:15 +0800

    no message

--- SwiftBrick/CollectionViewController.swift
+++ SwiftBrick/CollectionViewController.swift
@@ -15,6 +15,8 @@ class CollectionViewController: JHCollectionViewController {
         self.prefersNavigationBarHidden = false
         self.mainDatas = [["","","","","","","",""],["","","","","","",""],["","","","","","","","","","",""],["","","","","","","","","",""]]
         // Do any additional setup after loading the view.
+        addRightBarButton(normalImage: UIImage.init(named: "tab_chat_hi"), highLightImage: UIImage.init(named: "tab_home_hi"))
+        addRightBarButton(text: "123", normalColor: .darkGray, highlightColor: .lightGray)
         
         self.collectionView?.registerHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionHeader)
         self.collectionView?.registerHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionFooter)
