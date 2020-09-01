c086c8cd94fce2c5d2aa31371f496db248cf1262
Jackie <jackie>
2020-09-01 12:08:24 +0800

    添加设置部分圆角方法，UICollectionView支持重写setupFlowLayout配置自定义layout

--- SwiftBrick/CollectionViewController.swift
+++ SwiftBrick/CollectionViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class CollectionViewController: JHCollectionViewController {
 
-    override func setupFlowLayout() {
-        flowLayout = UICollectionViewFlowLayout.init()
-        flowLayout?.minimumLineSpacing = 0
-        flowLayout?.minimumInteritemSpacing = 0
-        flowLayout?.scrollDirection = UICollectionView.ScrollDirection.horizontal
+    override func setupFlowLayout() -> UICollectionViewFlowLayout{
+        let flowLayout = UICollectionViewFlowLayout.init()
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.scrollDirection = UICollectionView.ScrollDirection.vertical
+        return flowLayout
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,39 +30,29 @@ class CollectionViewController: JHCollectionViewController {
         self.collectionView?.registerHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionHeader)
         self.collectionView?.registerHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionFooter)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize.init(width: 200, height: 100)
     }
     
-//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-//        switch kind {
-//        case UICollectionView.elementKindSectionHeader:
-//            let header = collectionView.dequeueReusableHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionHeader, indexPath: indexPath)
-//            header.backgroundColor = .purple
-//            return header
-//        default:
-//            let footer = collectionView.dequeueReusableHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionFooter, indexPath: indexPath)
-//            footer.backgroundColor = .yellow
-//            return footer
-//        }
-//    }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-//        return CGSize.init(width: screen_width, height: 20)
-//    }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-//        return CGSize.init(width: screen_width, height: 60)
-//    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let header = collectionView.dequeueReusableHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionHeader, indexPath: indexPath)
+            header.backgroundColor = .purple
+            return header
+        default:
+            let footer = collectionView.dequeueReusableHeaderFooterView(JHCollectionReusableView.self, kindType: .sectionFooter, indexPath: indexPath)
+            footer.backgroundColor = .yellow
+            return footer
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize.init(width: ScreenWidth, height: 20)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize.init(width: ScreenWidth, height: 60)
+    }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(JHCollectionViewCell.self, indexPath: indexPath)
