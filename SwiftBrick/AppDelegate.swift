591668e4f1addc9acd676eb8fae9dfbe4eedc055
Jackie <jackie>
2021-11-19 18:54:13 +0800

    no message

--- SwiftBrick/AppDelegate.swift
+++ SwiftBrick/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        /////需要在APP启动之前调用此交换方法用于处理导航栏切换协调
         SwiftBrick.navBarSmooth()
         
         return true
