0e9aab21ed2c2c20ea35b20c59a0f5172e9410a8
Jackie <jackie>
2021-12-01 12:21:23 +0800

    no message

--- Sources/SwiftBrick/Extensions/UINavigationControllerEx.swift
+++ Sources/SwiftBrick/Extensions/UINavigationControllerEx.swift
@@ -24,23 +24,6 @@ public class SwizzleNavBar {
     }()
 }
 
-public extension NSObject {
-
-    static func swizzlingForClass(_ forClass: AnyClass, originalSelector: Selector, swizzledSelector: Selector) {
-        guard let originalMethod = class_getInstanceMethod(forClass, originalSelector),
-              let swizzledMethod = class_getInstanceMethod(forClass, swizzledSelector) else {
-            return
-        }
-
-        let isAddSuccess = class_addMethod(forClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
-        if isAddSuccess {
-            class_replaceMethod(forClass, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod)
-        }
-    }
-}
-
 public extension UIViewController {
  
     private struct Associated {
