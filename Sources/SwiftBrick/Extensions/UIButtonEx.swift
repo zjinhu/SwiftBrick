c594c227754995c639e1def7b5595cfce6335a0e
狄烨 <zhangjinhu>
2022-03-03 18:55:53 +0800

    no message

--- Sources/SwiftBrick/Extensions/UIButtonEx.swift
+++ Sources/SwiftBrick/Extensions/UIButtonEx.swift
@@ -245,8 +245,6 @@ public extension UIButton {
         }
     }
     
-    
-    
     @IBInspectable
     var backImageForNormal: UIImage? {
         get {
@@ -287,4 +285,24 @@ public extension UIButton {
         }
     }
 
+}
+
+public extension UIButton {
+    private func image(withColor color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        self.setBackgroundImage(image(withColor: color), for: state)
+    }
 }
