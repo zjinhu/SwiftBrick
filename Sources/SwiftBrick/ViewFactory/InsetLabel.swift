678147c4ece3bdb9dd875302402df304391b6859
虎 <814030966@qq.com>
2022-06-06 15:54:12 +0800

    release

--- Sources/SwiftBrick/ViewFactory/InsetLabel.swift
+++ Sources/SwiftBrick/ViewFactory/InsetLabel.swift
@@ -10,7 +10,7 @@ import UIKit
 // MARK: ===================================工厂类:带内边距UILabel=========================================
 public class InsetLabel: UILabel {
     // 1.定义一个接受间距的属性
-    var textInsets = UIEdgeInsets.zero
+    public var textInsets = UIEdgeInsets.zero
     
     //2. 返回 label 重新计算过 text 的 rectangle
     public override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
