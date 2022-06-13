49d675ef4db5b5757bb194bb5e54a1f7ee74e860
虎 <814030966@qq.com>
2022-06-13 14:50:40 +0800

    release

--- Sources/SwiftBrick/Extensions/LayoutEx.swift
+++ Sources/SwiftBrick/Extensions/LayoutEx.swift
@@ -14,13 +14,30 @@ public extension CGFloat {
     /// - Parameter size: origin width
     /// - Returns: 比例缩放后的 width 没有除以2.0
     func scaleWidth() -> CGFloat {
-        return (ScreenWidth / 375 * self)
+        return ScreenWidth / 375 * self
     }
     /// 比例缩放 height
     ///
     /// - Parameter size: origin height
     /// - Returns: 比例缩放后的 height 没有除以2.0
     func scaleHeight() -> CGFloat {
-        return (ScreenHeight / 667 * self)
+        return ScreenHeight / 667 * self
+    }
+}
+
+public extension Int {
+    /// 比例缩放 width
+    ///
+    /// - Parameter size: origin width
+    /// - Returns: 比例缩放后的 width 没有除以2.0
+    func scaleWidth() -> CGFloat {
+        return ScreenWidth / 375 * CGFloat(self)
+    }
+    /// 比例缩放 height
+    ///
+    /// - Parameter size: origin height
+    /// - Returns: 比例缩放后的 height 没有除以2.0
+    func scaleHeight() -> CGFloat {
+        return ScreenHeight / 667 * CGFloat(self)
     }
 }
--- SwiftBrick.podspec
+++ SwiftBrick.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
 s.name             = 'SwiftBrick'
-s.version          = '1.9.9'
+s.version          = '2.0.0'
 s.summary          = '工具类组件.'
 
 s.description      = <<-DESC
