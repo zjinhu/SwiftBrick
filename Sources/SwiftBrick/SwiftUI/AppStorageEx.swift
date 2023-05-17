3da6c80a71345ef8c80f29c3a74dc18353b5846a
jinhu <814030966@qq.com>
2023-05-17 16:52:48 +0800

    添加设备型号

--- Sources/SwiftBrick/SwiftUI/AppStorageEx.swift
+++ Sources/SwiftBrick/SwiftUI/AppStorageEx.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+//import SwiftUI
 ///增加@AppStorage 支持
 extension Date: RawRepresentable{
     public typealias RawValue = String
@@ -64,3 +65,27 @@ extension Array: RawRepresentable where Element: Codable {
         return result
     }
 }
+
+//@propertyWrapper
+//public struct Default<T>: DynamicProperty {
+//    @ObservedObject private var defaults: Defaults
+//    private let keyPath: ReferenceWritableKeyPath<Defaults, T>
+//    public init(_ keyPath: ReferenceWritableKeyPath<Defaults, T>, defaults: Defaults = .shared) {
+//        self.keyPath = keyPath
+//        self.defaults = defaults
+//    }
+//
+//    public var wrappedValue: T {
+//        get { defaults[keyPath: keyPath] }
+//        nonmutating set { defaults[keyPath: keyPath] = newValue }
+//    }
+//
+//    public var projectedValue: Binding<T> {
+//        Binding(
+//            get: { defaults[keyPath: keyPath] },
+//            set: { value in
+//                defaults[keyPath: keyPath] = value
+//            }
+//        )
+//    }
+//}
--- Sources/SwiftBrick/Util/Device.swift
+++ Sources/SwiftBrick/Util/Device.swift
@@ -155,5 +155,18 @@ extension SwiftBrick{
             }
             return false
         }
+        
+        public static var identifier: String = {
+           var systemInfo = utsname()
+           uname(&systemInfo)
+           let mirror = Mirror(reflecting: systemInfo.machine)
+
+           let identifier = mirror.children.reduce("") { identifier, element in
+             guard let value = element.value as? Int8, value != 0 else { return identifier }
+             return identifier + String(UnicodeScalar(UInt8(value)))
+           }
+           return identifier
+         }()
+
     }
 }
